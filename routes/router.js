910aef455920d8334d76004a573abc9815d6998d
gilmoreg@live.com <gilmoreg@live.com>
2017-02-01 09:47:39 -0600

    Fix exports in router

--- routes/router.js
+++ routes/router.js
@@ -7,3 +7,19 @@ const morgan = require('morgan');
 const {logger} = require('../logger');
 const {References, Articles, Books, Websites} = require('../models/reference');
 
+router.get('/', (req, res) => {
+	logger.log('info',`GET ${req}`);
+	 
+ 	BlogPosts
+		.find()
+		.exec() 
+		.then( (posts) => {
+			res.json({posts: posts.map((post)=>{return post.json();})});
+		})
+		.catch( err => {
+			logger.log('error',err);
+			res.status(500).json({message:'Internal server error'});
+		})
+});
+
+module.exports = router;
