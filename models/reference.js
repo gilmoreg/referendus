2eaa48b83ac84518707fa7d92b61cd471e2e6043
gilmoreg@live.com <gilmoreg@live.com>
2017-02-01 09:31:06 -0600

    Add models

---
+++ models/reference.js
@@ -0,0 +1,106 @@
+const mongoose = require('mongoose');
+
+const options = { discriminatorKey: 'type' };
+
+const refSchema = mongoose.Schema( {
+    type: { type: String, required:true },
+    title: { type: String, required:true },
+    tags: [ { tag:String } ],
+    id: String,
+    notes: String
+}, options);
+
+const articleSchema = mongoose.Schema( {
+    authors: 
+        { type: [{
+            author: {
+                firstName: String,
+                middleName: String,
+                lastName: String,
+            }
+        }], required:true 
+    },
+    year: { type: Number, required:true },
+    volume: { type: Number, required:true },
+    issue: { type:Number, required:true },
+    pages: { type:String, required:true },
+    url: String
+}, options);
+
+const bookSchema = mongoose.Schema( {
+    authors: 
+        { type: [{
+            author: {
+                firstName: String,
+                middleName: String,
+                lastName: String,
+            }
+        }], required:true 
+    },
+    city: { type:String, required:true },
+    publisher: { type:String, required:true },
+    year: { type:Number, required:true },
+    edition: String,
+    url:String
+}, options);
+
+const websiteSchema = mongoose.Schema( {
+    authors: [{
+            author: {
+                firstName: String,
+                middleName: String,
+                lastName: String,
+            }
+        }],
+    siteTitle: { type:String, required:true },
+    accessDate: { type:Date, required:true },
+    pubDate: Date,
+    url: { type:String, required:true }
+}, options);
+
+/*
+    authors: [{
+        author: {
+            firstName: String,
+            middleName: String,
+            lastName: String,
+        }
+    }],
+    year: Number,
+    volume: Number,
+    issue: Number,
+    pages: String,
+    url: String,
+    id: String,
+    tags: [ { tag:String } ],
+    notes: String,
+    edition: String,
+    city: String,
+    publisher: String,
+    pubDate: Date,
+    accessDate: Date,
+
+*/
+
+/*refSchema.virtual('authorFullName').get( function() {
+  return `${this.author.firstName} ${this.author.lastName}`;
+});*/
+
+refSchema.methods.json = function() {
+  return {
+    /*
+        id: this._id,
+        title: this.title,
+        content: this.content,
+        author: this.authorFullName,
+        created: this.created
+    */
+  };
+};
+
+const References = mongoose.model('Reference', refSchema);
+const Books = References.discriminator('Book', bookSchema);
+const Articles = References.discriminator('Article', articleSchema);
+const Websites = References.discriminator('Website', websiteSchema);
+
+module.exports = {References, Articles, Books, Websites};
--- test/test-db.js
+++ test/test-db.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
+// TODO is this necessary?
 const morgan = require('morgan');
 const {logger} = require('../logger');
 
@@ -9,6 +10,7 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+// TODO is this necessary?
 app.use(morgan('common', {stream: logger.stream}));
 
 function tearDownDb() {
