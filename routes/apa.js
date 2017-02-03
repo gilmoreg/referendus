61150611b6c9f8f8e9f20a34b351c1f7547a8bf8
gilmoreg@live.com <gilmoreg@live.com>
2017-02-03 14:28:10 -0600

    Convert apa.js to ES6

--- routes/apa.js
+++ routes/apa.js
@@ -13,13 +13,13 @@ const res400Err = (msg, res) => {
 }
 
 // https://owl.english.purdue.edu/owl/resource/560/06/
-var apaName = function(author) {
-	var str = author.lastName + ', ' + author.firstName.charAt(0) + '.';
-	if(author.middleName) str += author.middleName.charAt(0) + '.';
+const apaName = author => {
+	var str = `${author.lastName}, ${author.firstName.charAt(0)}.`;
+	if(author.middleName) str += `${author.middleName.charAt(0)}.`;
 	return str;
 }
 
-var generateAPAAuthorList = function(authors) {
+const generateAPAAuthorList = authors => {
 	console.log('generateAPAAuthorList',authors);
 	if(authors.length<1) return '';
 	var str='';
@@ -29,53 +29,52 @@ var generateAPAAuthorList = function(authors) {
 	}
 	// Two authors
 	else if(authors.length===2) {
-		str += apaName(authors[0].author) + '& ' + apaName(authors[1].author);		
+		str += `${apaName(authors[0].author)} & ${apaName(authors[1].author)}`;		
 	}
 	// Between 3 and 7 authors
 	else if(authors.length>=3 && authors.length<=7) {
 		// Last author has to be preceded by ampersand, so count up to penultimate only
 		for(var i=0; i<authors.length-1;i++) {
-			str += apaName(authors[i].author) + ', ';
+			str += `${apaName(authors[i].author)}, `;
 		}
-		str += '& ' + apaName(authors[authors.length-1].author) + '. ';
+		str += `& ${apaName(authors[authors.length-1].author)}. `;
 	}
 	// More than 7 authors
 	else if(authors.length>7) {
 		for(var i=0; i<6;i++) {
-			str += apaName(authors[i].author) + ', ';
+			str += `${apaName(authors[i].author)}, `;
 		}
-		str += '. . . ' + apaName(authors[authors.length-1].author);
+		str += `. . . ${apaName(authors[authors.length-1].author)}`;
 	}
 	return str;
 }
 
-var generateAPAArticle = function(ref) {
+const generateAPAArticle = ref => {
 	var str = '';
-	str += generateAPAAuthorList(ref.authors) + ' ';
-	str += '(' + ref.year + '). ' + ref.title + '. ' + '<i>' + ref.journal + '</i>, ';
+	str += `${generateAPAAuthorList(ref.authors)} `;
+	str += `(${ref.year}). ${ref.title}. <i>${ref.journal}</i>, `;
 	str += '<i>' + ref.volume + '</i>';
-	if(ref.pages) str += ', ' + ref.pages + '.';
+	if(ref.pages) str += `, ${ref.pages}.`;
 	else str += '.';
 	return str;
 }
 
-var generateAPABook = function(ref) {
+const generateAPABook = ref => {
 	var str = '';
-	str += generateAPAAuthorList(ref.authors) + ' ';
-	str += '(' + ref.year + '). <i>' + ref.title + '</i>. ';
-	str += ref.city + ': ' + ref.publisher + '.';
+	str += `${generateAPAAuthorList(ref.authors)} (${ref.year}). <i>${ref.title}</i>. `;
+	str += `${ref.city}: ${ref.publisher}.`;
 	return str;
 }
 
-var generateAPAWebsite = function(ref) {
+const generateAPAWebsite = ref => {
     var str = '';
-    str += generateAPAAuthorList(ref.authors) + ' ';
-    if(ref.pubDate) str += '(' + ref.pubDate + '). ';
-    str += ref.title + '. <i>' + ref.siteTitle + '</i>. Retrieved ' + ref.accessDate + ' from ' + ref.url + '.';
+    str += `${generateAPAAuthorList(ref.authors)} `;
+    if(ref.pubDate) str += `(${ref.pubDate}). `;
+    str += `${ref.title}, <i>${ref.siteTitle}</i>. Retrieved ${ref.accessDate} from ${ref.url}.`;
     return str;
 }
 
-const generateReference = (ref) => {
+const generateReference = ref => {
     switch(ref.type) {
         case 'Article': { return generateAPAArticle(ref); break; }
         case 'Book': { return generateAPABook(ref); break; }
