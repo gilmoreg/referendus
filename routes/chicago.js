2bd074bc0f4d0ba23095b6589396eff59b5dd39c
gilmoreg@live.com <gilmoreg@live.com>
2017-02-03 15:05:42 -0600

    Add authorList function

--- routes/chicago.js
+++ routes/chicago.js
@@ -7,14 +7,31 @@ const morgan = require('morgan');
 const {logger} = require('../logger');
 const {References/*, Articles, Books, Websites*/} = require('../models/reference');
 
-// http://www.press.uchicago.edu/books/turabian/turabian_citationguide.html
 const res400Err = (msg, res) => {
     logger.log('error',msg);
     return res.status(400).send(msg);
 }
 
-const cName = (author) => {
+// http://www.press.uchicago.edu/books/turabian/turabian_citationguide.html
+const cName = author => {
+    let str = `${author.lastName}, ${author.firstName}`;
+    if(author.middleName) str += `${author.middleName.charAt(0)}`;
+    return str;
+}
 
+const authorList = authors => {
+    if(authors.length<1) return '';
+    if(authors.length===1) {
+        return `${cName(authors[0])}.`;
+    }
+    else {
+        let str='';
+        // Last author has to be preceded by 'and', so count up to penultimate only
+        for(let i=0;i<authors.length-1;i++) {
+            str += `${cName(authors[i])}, `;
+        }
+        str += `and ${cName(authors[authors.length])}.`;
+    }
 }
 
 const generateChicagoArticle = ref => {
