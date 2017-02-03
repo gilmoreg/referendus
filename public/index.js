593dc1e3580e9fc2cbd3cd42592fa4bc2968945a
gilmoreg@live.com <gilmoreg@live.com>
2017-02-03 13:20:43 -0600

    Add APA generators

--- public/index.js
+++ public/index.js
@@ -5,6 +5,124 @@ var formError = function(msg) {
 	$('.modal-message').hide().html(msg).slideDown(100).delay(5000).fadeOut(100);
 }
 
+// To Mark: there has to be a better way?
+var generateReference = function(ref) {
+	switch(format) {
+		case 'APA': {
+			switch(ref.type) {
+				case 'Article': { return generateAPAArticle(ref); break; }
+				case 'Book': { return generateAPABook(ref); break; }
+				case 'Website': { return generateAPAWebsite(ref); break; }
+			}
+			break;
+		 };
+		case 'Chicago': {
+			switch(ref.type) {
+				case 'Article': { return generateChicagoArticle(ref); break; }
+				case 'Book': { return generateChicagoBook(ref); break; }
+				case 'Website': { return generateChicagoWebsite(ref); break; }
+			}
+			break;
+		}
+		case 'MLA': {
+			switch(ref.type) {
+				case 'Article': { return generateMLAArticle(ref); break; }
+				case 'Book': { return generateMLABook(ref); break; }
+				case 'Website': { return generateMLAWebsite(ref); break; }
+			}
+			break;
+		}
+		// TODO error
+		default: { return ''; }
+	}
+}
+
+var firstFullLastInitials = function(author) {
+	var str = author.lastName + ', ' + author.firstName.charAt(0) + '.';
+	if(author.middleName) str += author.middleName.charAt(0) + '.';
+	return str;
+}
+
+var generateAPAAuthorList = function(authors) {
+	if(authors.length<1) return '';
+	var str='';
+	// Single author
+	if(authors.length===1) {
+		str += firstFullLastInitials(authors[0]);
+	}
+	// Two authors
+	else if(authors.length===2) {
+		str += firstFullLastInitials(authors[0]) + '& ' + firstFullLastInitials(authors[1]);		
+	}
+	// Between 3 and 7 authors
+	else if(authors.length>=3 && authors.length<=7) {
+		// Last author has to be preceded by ampersand, so count up to penultimate only
+		for(var i=0; i<authors.length-1;i++) {
+			str += firstFullLastInitials(authors[i]) + ', ';
+		}
+		str += '& ' + firstFullLastInitials(authors[authors.length-1]) + '. ';
+	}
+	// More than 7 authors
+	else if(authors.length>7) {
+		for(var i=0; i<6;i++) {
+			str += firstFullLastInitials(authors[i]) + ', ';
+		}
+		str += '. . . ' + firstFullLastInitials(authors[authors.length-1]);
+	}
+	return str;
+}
+
+// https://owl.english.purdue.edu/owl/resource/560/06/
+var generateAPAArticle = function(ref) {
+	var str = '';
+	str += generateAPAAuthorList(ref.authors) + ' ';
+	str += '(' + ref.year + '). ' + ref.title + '. ' + '<i>' + ref.journal + '</i>, ';
+	str += '<i>' + ref.volume + '</i>';
+	if(ref.pages) str += ', ' + ref.pages + '.';
+	else str += '.';
+	return str;
+}
+
+var generateAPABook = function(ref) {
+	var str = '';
+	str += generateAPAAuthorList(ref.authors) + ' ';
+	str += '(' + ref.year + '). <i>' + ref.title + '</i>. ';
+	str += ref.city + ': ' + ref.publisher + '.';
+	return str;
+}
+
+var generateAPAWebsite = function(ref) {
+  var str = '';
+  str += generateAPAAuthorList(ref.authors) + ' ';
+  if(ref.pubDate) str += '(' + ref.pubDate + '). ';
+  str += ref.title + '. <i>' + ref.siteTitle + '</i>. Retrieved ' + ref.accessDate + ' from ' + ref.url + '.';
+  return str;
+}
+
+var generateChicagoArticle = function(ref) {
+
+}
+
+var generateChicagoBook = function(ref) {
+
+}
+
+var generateChicagoWebsite = function(ref) {
+	
+}
+
+var generateMLAArticle = function(ref) {
+
+}
+
+var generateMLABook = function(ref) {
+
+}
+
+var generateMLAWebsite = function(ref) {
+	
+}
+
 var refreshList = function() {
 	$('.ref-container').empty();
 	$.ajax({
