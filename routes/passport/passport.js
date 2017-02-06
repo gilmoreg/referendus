d91e0760d93493247a3bd7e86dbe47d8891f4f73
gilmoreg@live.com <gilmoreg@live.com>
2017-02-06 16:32:00 -0600

    Remove debuggers

--- routes/passport/passport.js
+++ routes/passport/passport.js
@@ -8,36 +8,23 @@ const {logger} = require('../../logger');
 
 passport.use(new LocalStrategy(
   (username, password, callback) => {
-    logger.log('info','attempting LocalStrategy');
     User.findOne({ username: username }, (err, user) => {
-      console.log('info','attempting findOne err', err);
-      console.log('info','attempting findOne user', user);
       if (err) { 
-        console.log('info','LocalStrategy',err,user);
         return callback(err); 
       }
-
       // No user found with that username
       if (!user) { 
-        console.log('info','LocalStrategy',err,user);
         return callback(null, false); 
       }
-      console.log('going to check password');
       // Make sure the password is correct
       user.validatePassword(password, (err, isMatch) => {
-        // We never make it here
-        console.log('info','validatePassword',err,isMatch);
         if (err) { 
-          console.log('info','validatePassword',err,isMatch);
           return callback(err); 
         }
-
         // Password did not match
         if (!isMatch) { 
-          console.log('info','validatePassword',err,match);
           return callback(null, false); 
         }
-        console.log('info','validatePassword success',user);
         // Success
         return callback(null, user);
       });
