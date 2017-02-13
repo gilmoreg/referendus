3479a494ddd1a11c2108c8b59a19b1b974dfc6b8
gilmoreg@live.com <gilmoreg@live.com>
2017-02-13 14:33:44 -0600

    Add tag search route Chicago; cleanup

--- routes/formats/chicago.js
+++ routes/formats/chicago.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 const {logger} = require('../../logger');
 const moment = require('moment');
 const {References/*, Articles, Books, Websites*/} = require('../../models/reference');
@@ -10,21 +7,21 @@ const {References/*, Articles, Books, Websites*/} = require('../../models/refere
 const res400Err = (msg, res) => {
     logger.log('error',msg);
     return res.status(400).send(msg);
-}
+};
 
 // http://www.press.uchicago.edu/books/turabian/turabian_citationguide.html
 const lastFirst = author => {
     let str = `${author.lastName}, ${author.firstName}`;
     if(author.middleName) str += ` ${author.middleName.charAt(0)}`;   
     return str;
-}
+};
 
 const firstLast = author => {
     let str = author.firstName;
     if(author.middleName) str += `${author.middleName.charAt(0)}.`;
     str += ` ${author.lastName}`;
     return str;
-}
+};
 
 const authorList = authors => {
     if(authors.length<1) return '';
@@ -39,8 +36,9 @@ const authorList = authors => {
             str += `${firstLast(authors[i].author)}, `;
         }
         str += `and ${firstLast(authors[authors.length].author)}.`;
+        return str;
     }
-}
+};
 
 const article = ref => {
 	var str = authorList(ref.authors);
@@ -48,7 +46,7 @@ const article = ref => {
     if(ref.pages) str += `: ${ref.pages}.`;
     else str += '.';
 	return { data:ref, html:str };
-}
+};
 
 const book = ref => {
 	var str = authorList(ref.authors);
@@ -56,7 +54,7 @@ const book = ref => {
     if(ref.pages) str += `, ${ref.pages}`;
     str += '.';
 	return { data:ref, html:str };
-}
+};
 
 const website = ref => {
 	const authors = authorList(ref.authors);
@@ -73,28 +71,28 @@ const website = ref => {
     }
     str += `${ref.url}.`;
 	return { data:ref, html:str };
-}
+};
 
 const generateReference = ref => {
     switch(ref.type) {
-        case 'Article': { return article(ref); break; }
-        case 'Book': { return book(ref); break; }
+        case 'Article': { return article(ref); }
+        case 'Book': { return book(ref); }
         case 'Website': { return website(ref); }
     }
-}
+};
 
 const isAuthenticated = (req, res, next) => {
 	if(req.isAuthenticated()){
         return next();
     } else{
-		res.redirect("/");
+		res.redirect('/');
     }
-}
+};
 
 router.get('/', isAuthenticated, (req, res) => {
 	logger.log('info',`GET /refs/chicago ${req}`);
-	 
- 	References
+
+    References
 		.find({'user':req.user._doc.username})
 		.exec() 
 		.then( (refs) => {
@@ -103,7 +101,22 @@ router.get('/', isAuthenticated, (req, res) => {
 		.catch( err => {
 			logger.log('error',err);
 			res.status(500).json({message:'Internal server error'});
+		});
+});
+
+router.get('/search/:tag', isAuthenticated, (req, res) => {
+	logger.log('info',`GET /refs/apa/search ${req}`);
+	if(!req.params.tag) return res400Err('Missing "tag" in params');
+	References
+		.find({'user':req.user._doc.username, 'tag':req.params.tag})
+		.exec() 
+		.then( (refs) => {
+			res.json({refs: refs.map((ref)=>{return generateReference(ref);})});
 		})
+		.catch( err => {
+			logger.log('error',err);
+			res.status(500).json({message:'Internal server error'});
+		});
 });
 
 module.exports = router;
