0a3ba5a44780dcc4e68acd0944e4673a575afbae
Grayson Gilmore <gilmoreg@live.com>
2017-07-13 18:26:17 -0500

    Lint user schema

--- models/user.js
+++ models/user.js
@@ -1,43 +1,42 @@
+/* eslint-disable func-names */
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 const UserSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
 });
 
-UserSchema.methods.json = function() {
+UserSchema.methods.json = function () {
   return {
-    username: this.username || ''
+    username: this.username || '',
   };
 };
 
-UserSchema.methods.validatePassword = function(password, cb) {
+UserSchema.methods.validatePassword = function (password, cb) {
   bcrypt.compare(password, this.password, (err, res) => {
-    if(err) {
+    if (err) {
       cb(err, false);
     }
-    if(res) {
+    if (res) {
       cb(null, true);
-    }
-    else {
+    } else {
       cb(err, false);
     }
   });
 };
 
-UserSchema.statics.hashPassword = function(password) {
-  return bcrypt.hash(password, 10);
-};
+UserSchema.statics.hashPassword = password => bcrypt.hash(password, 10);
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = { User };
