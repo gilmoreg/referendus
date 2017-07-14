897e7d401a11ad511aa51dff720ea6db8ae8c0e0
Grayson Gilmore <gilmoreg@live.com>
2017-07-13 20:38:30 -0500

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const routes = require('./passport/passport');
 
-router.use(require('./passport/passport'));
+router.use(routes);
 router.use('/auth', require('./passport/auth'));
 router.use('/refs', require('./refs'));
 router.use('/ref', require('./ref'));
 router.use('/refs/apa', require('./formats/apa'));
 router.use('/refs/chicago', require('./formats/chicago'));
 router.use('/refs/mla', require('./formats/mla'));
-
 
 module.exports = { router };
