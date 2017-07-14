4fac1508492219253a156bbe4e3bd62f1f66ec99
Grayson Gilmore <gilmoreg@live.com>
2017-07-13 20:40:40 -0500

    Cleanup

--- routes/passport/passport.js
+++ routes/passport/passport.js
@@ -8,40 +8,39 @@ const LocalStrategy = require('passport-local').Strategy;
 const { User } = require('../../models/user');
 const { DATABASE_URL } = require('../../config');
 
-passport.use(new LocalStrategy(
-  (username, password, callback) => {
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        return callback(err);
+const strategy = new LocalStrategy((username, password, callback) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return callback(err);
+    }
+    // No user found with that username
+    if (!user) {
+      return callback(null, false);
+    }
+    // Make sure the password is correct
+    return user.validatePassword(password, (validateErr, isMatch) => {
+      if (validateErr) {
+        return callback(validateErr);
       }
-      // No user found with that username
-      if (!user) {
+      // Password did not match
+      if (!isMatch) {
         return callback(null, false);
       }
-      // Make sure the password is correct
-      return user.validatePassword(password, (validateErr, isMatch) => {
-        if (validateErr) {
-          return callback(validateErr);
-        }
-        // Password did not match
-        if (!isMatch) {
-          return callback(null, false);
-        }
-        // Success
-        return callback(null, user);
-      });
+      // Success
+      return callback(null, user);
     });
-  },
-));
+  });
+});
 
-router.use(session(
-  {
-    secret: 'keyboard puma',
-    resave: true,
-    saveUninitialized: true,
-    store: new MongoStore({ url: DATABASE_URL }),
-  },
-));
+passport.use(strategy);
+
+router.use(session({
+  secret: 'keyboard puma',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({ url: DATABASE_URL }),
+}));
+
 router.use(passport.initialize());
 router.use(passport.session());
 
